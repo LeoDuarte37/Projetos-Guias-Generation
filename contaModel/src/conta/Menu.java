b25fad6a8a222de8b8daedcae88be96a1717b61b
Leo <leonardo.r.s.duarte@gmail.com>
2023-12-14 15:55:19 -0300

    Projeto Guia - Conta model

--- contaModel/src/conta/Menu.java
+++ contaModel/src/conta/Menu.java
@@ -1,17 +1,26 @@
 package conta;
 
 import java.util.Scanner;
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
-		
-		Scanner ler = new Scanner(System.in);
-		
-		int opcao = 0;
-		
-		while(opcao != 9) {	
+		// Teste da Classe Conta
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+
+		Scanner leia = new Scanner(System.in);
+
+		int opcao;
+
+		while (true) {
+
 			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
 					+ "*****************************************************");
 			System.out.println("                                                     ");
@@ -32,69 +41,63 @@ public class Menu {
 			System.out.println("*****************************************************");
 			System.out.println("Entre com a opção desejada:                          ");
 			System.out.println("                                                     " + Cores.TEXT_RESET);
-			
-			opcao = ler.nextInt();			
-			
+
+			opcao = leia.nextInt();
+
 			if (opcao == 9) {
-				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu futuro começa aqui");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
 				sobre();
-				ler.close();
+				leia.close();
 				System.exit(0);
 			}
-			
-			switch(opcao) {
+
+			switch (opcao) {
 			case 1:
-				System.out.println(Cores.TEXT_WHITE_BOLD + "\nCriar conta");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Criar Conta\n\n");
+
 				break;
-			
 			case 2:
-				System.out.println(Cores.TEXT_WHITE_BOLD + "\nListar todas as contas");			
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as Contas\n\n");
+
 				break;
-				
 			case 3:
-				System.out.println(Cores.TEXT_WHITE_BOLD + "\nConsultar dados da conta - por número");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da Conta - por número\n\n");
+
 				break;
-				
 			case 4:
-				System.out.println(Cores.TEXT_WHITE_BOLD + "\nAtualizar dados da conta");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da Conta\n\n");
+
 				break;
-				
 			case 5:
-				System.out.println(Cores.TEXT_WHITE_BOLD + "\nApagar a conta");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a Conta\n\n");
+
 				break;
-				
 			case 6:
-				System.out.println(Cores.TEXT_WHITE_BOLD + "\nSaque");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\n\n");
+
 				break;
-				
 			case 7:
-				System.out.println(Cores.TEXT_WHITE_BOLD + "\nDepósito");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\n\n");
+
 				break;
-				
 			case 8:
-				System.out.println(Cores.TEXT_WHITE_BOLD + "\nTranferência entre contas");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre Contas\n\n");
+
 				break;
-				
 			default:
-				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção inválida!");
+				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n");
 				break;
 			}
 		}
-		
-		
+
 	}
-
-	public static void sobre() {
-		System.out.println("\n---------------------------------------------------\n");
-		System.out.println("Projeto desenvolvido por: Leonardo Duarte");
-		System.out.println("Generation Brasil - leonardo.r.s.duarte@gmail.com");
-		System.out.println("github.com/LeoDuarte37/Projetos-Guias-Generation");
-		System.out.println("\n---------------------------------------------------");
-	}
-}
-
-
-
-
-
 	
+	public static void sobre() {
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por: ");
+		System.out.println("Generation Brasil - generation@generation.org");
+		System.out.println("github.com/conteudoGeneration");
+		System.out.println("*********************************************************");
+	}
+
+}
